9b05b960b27ca695e9d99684f33eb2b0d8824e19
Jonathon Pemberton <jonpemby@icloud.com>
2019-01-09 14:09:20 -0600

    Create a Digital Ocean service provider. (#5)
    Closes #4.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,12 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use GuzzleHttp\Client;
-use App\DigitalOcean\Connector;
-use App\DigitalOcean\Contracts\Connector as ConnectorInterface;
-use App\DigitalOcean\Contracts\Decorator;
-use App\DigitalOcean\Decorators\ProfileConnector;
-use App\DigitalOcean\Decorators\CachedConnector;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,23 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->app->bind(ConnectorInterface::class, Connector::class);
-
-        $this->app->when(Connector::class)
-             ->needs(Client::class)
-             ->give(function () {
-                 return new Client(['base_uri' => config('digitalocean.api_url')]);
-             });
-
-        $this->app->bind(Decorator::class, function ($app) {
-            $connector = $app->make(ConnectorInterface::class);
-
-            if (config('digitalocean.cache')) {
-                return new CachedConnector($connector);
-            }
-
-            return new ProfileConnector($connector);
-        });
+        //
     }
 
     /**
---
+++ app/Providers/DigitalOceanServiceProvider.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use GuzzleHttp\Client;
+use App\DigitalOcean\Connector;
+use App\DigitalOcean\Contracts\Connector as ConnectorInterface;
+use App\DigitalOcean\Contracts\Decorator;
+use App\DigitalOcean\Decorators\ProfileConnector;
+use App\DigitalOcean\Decorators\CachedConnector;
+
+class DigitalOceanServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->app->bind(ConnectorInterface::class, Connector::class);
+
+        $this->app->when(Connector::class)
+             ->needs(Client::class)
+             ->give(function () {
+                 return new Client(['base_uri' => config('digitalocean.api_url')]);
+             });
+
+        $this->app->bind(Decorator::class, function ($app) {
+            $connector = $app->make(ConnectorInterface::class);
+
+            if (config('digitalocean.cache')) {
+                return new CachedConnector($connector);
+            }
+
+            return new ProfileConnector($connector);
+        });
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
